fa1d7b3b104c40955437c63cf936d91912615cdd
Manuel Wegria <abstract.s33d@gmail.com>
2018-08-10 18:52:58 +0200

    tidying js packs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,20 +6,6 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
-console.log('Hello World from Webpacker')
 
 
-import Typed from 'typed.js';
-
-var options = {
-  strings: ["Try me out ;)", "Search by name", "Or by Ingredient", ""],
-  typeSpeed: 25
-}
-const typed1 = new Typed(".search-input-1", options);
-const typed2 = new Typed(".search-input-2", options);
-
-
-
-
-import AOS from 'aos';
-AOS.init();
+//console.log('Hello World from Webpacker')
--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,4 +1,3 @@
-
 function build_container(parent_el, containing_el) {
   let parent = document.querySelector(`#${parent_el} .first`);
   let container = document.createElement('div');;
@@ -7,7 +6,6 @@ function build_container(parent_el, containing_el) {
   parent.parentNode.insertBefore( container, parent.nextSibling );
   return container = document.querySelector(`div#${parent_el} div#${containing_el} .first`);
 }
-
 function dynamic_hover_chars(parent_el, containing_el, toggle_class, string){
   let container = build_container(parent_el, containing_el);
   Array.from(string).forEach((l, i) => {
@@ -17,24 +15,18 @@ function dynamic_hover_chars(parent_el, containing_el, toggle_class, string){
     letter.setAttribute('data-letter', `${l}_${i}`);
     container.parentNode.insertBefore( letter, container );
   })
-
   function removeTransition(e){
     if (e.propertyName !== 'transform') return;
     this.classList.remove(toggle_class);
   }
-
   const letters = document.querySelectorAll('.letter');
   document.addEventListener('mouseover', e => {
     const letter = document.querySelector(`div#${parent_el} div#${containing_el} div[data-letter=${e.target.dataset.letter}]`);
     if (!letter) return;
     letter.classList.add(toggle_class);
   })
-
   letters.forEach(l => l.addEventListener('transitionend', removeTransition));
 }
-
-
-
 function init() {
   let element = document.querySelector(`div#content div#title`);
   if (element) {
@@ -42,6 +34,19 @@ function init() {
   }
   dynamic_hover_chars('content', 'title', 'hovering', 'Cocktailicious')
 }
-
-
 document.addEventListener('DOMContentLoaded', init(), false);
+
+
+
+
+import Typed from 'typed.js';
+var options = {
+  strings: ["Try me out ;)", "Search by name", "Or by Ingredient", ""],
+  typeSpeed: 25
+}
+const typed1 = new Typed(".search-input-1", options);
+const typed2 = new Typed(".search-input-2", options);
+
+
+import AOS from 'aos';
+AOS.init();
