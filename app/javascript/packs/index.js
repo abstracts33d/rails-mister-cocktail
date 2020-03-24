95c6df54ffb944c1536aef7edf58c883f2ea399d
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-24 10:16:56 +0100

    update index.js with improved version of dynamic_hover_chars

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,6 +1,9 @@
+import Typed from 'typed.js';
+import AOS from 'aos';
+
 function build_container(parent_el, containing_el) {
   let parent = document.querySelector(`#${parent_el} .first`);
-  let container = document.createElement('div');;
+  let container = document.createElement('div');
   container.innerHTML = '<span class="first"></span>';
   container.id =`${containing_el}`;
   parent.parentNode.insertBefore( container, parent.nextSibling );
@@ -15,18 +18,30 @@ function dynamic_hover_chars(parent_el, containing_el, toggle_class, string){
     letter.setAttribute('data-letter', `${l}_${i}`);
     container.parentNode.insertBefore( letter, container );
   })
+
+  const letters = document.querySelectorAll('.letter');
+  let lettersTransitionState = [];
+
   function removeTransition(e){
     if (e.propertyName !== 'transform') return;
-    this.classList.remove(toggle_class);
+    lettersTransitionState[`${e.target.dataset.letter}`] = false;
+    if (this.classList.contains(toggle_class)){
+      this.classList.remove(toggle_class);
+      lettersTransitionState[`${e.target.dataset.letter}`] = true;
+    }
   }
-  const letters = document.querySelectorAll('.letter');
+
   document.addEventListener('mouseover', e => {
     const letter = document.querySelector(`div#${parent_el} div#${containing_el} div[data-letter=${e.target.dataset.letter}]`);
     if (!letter) return;
+    if (letter.classList.contains(toggle_class) || lettersTransitionState[`${e.target.dataset.letter}`] === true) return;
     letter.classList.add(toggle_class);
+    lettersTransitionState[`${e.target.dataset.letter}`] = true;
   })
+
   letters.forEach(l => l.addEventListener('transitionend', removeTransition));
 }
+
 function init() {
   let element = document.querySelector(`div#content div#title`);
   if (element) {
@@ -36,8 +51,6 @@ function init() {
 }
 document.addEventListener('DOMContentLoaded', init(), false);
 
-
-
 let cells = document.querySelectorAll(".grid-x .cell")
 
 cells.forEach((cell) => {
@@ -45,7 +58,6 @@ cells.forEach((cell) => {
 })
 
 
-import Typed from 'typed.js';
 var options = {
   strings: ["Try me out ;)", "Search by name", "Or by Ingredient", ""],
   typeSpeed: 25
@@ -54,5 +66,4 @@ const typed1 = new Typed(".search-input-1", options);
 const typed2 = new Typed(".search-input-2", options);
 
 
-import AOS from 'aos';
 AOS.init();
